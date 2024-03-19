3543909037a1c70f6b549de9b8b980ab05563b62
Stella <samspchan@gmail.com>
2024-03-19 15:19:50 -0300

    Commit da atividade de Wrappers da EBAC

---
+++ AtividadeWrappersEBAC/src/ebac/atividades/wrapper/AtividadeWrapper.java
@@ -0,0 +1,15 @@
+package ebac.atividades.wrapper;
+
+public class AtividadeWrapper {
+
+	public static void main(String args[]) {
+		
+		int valor1 = 10;
+		System.out.println("Variável primitiva: " + valor1);
+		
+		Integer valor2 = valor1;
+		System.out.println("Variável Wrapper:" + valor2);
+		
+	}
+	
+}
---
+++ AtividadeWrappersEBAC/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module AtividadeWrappersEBAC {
+}
